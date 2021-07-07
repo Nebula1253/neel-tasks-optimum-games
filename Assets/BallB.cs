e5a8419f2ab8740c97e85492751c2bfa02433f96
Neel Amonkar <s2030247@ed.ac.uk>
2021-07-07 16:58:58 +0530

    finished ball and container movement, as well as behaviour on collision

--- Assets/BallB.cs
+++ Assets/BallB.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class BallB : MonoBehaviour
 {
     public Rigidbody2D body;
+    private Vector2 startPos = new Vector2(0, 4);
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,8 +15,27 @@ public class BallB : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // shoots ball downward when the left mouse button is clicked
         if (Input.GetMouseButtonDown(0)){
             body.velocity = new Vector2(0, -10);
         }
+
+        // resets position if the ball goes straight past the container
+        if (transform.position.y <= -5)
+        {
+            resetPosition();
+        }
+    }
+
+    // not much point checking WHAT the object is colliding with since there's only one other thing in the scene
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        resetPosition();
+    }
+
+    void resetPosition()
+    {
+        body.velocity = new Vector2(0, 0);
+        transform.position = startPos;
     }
 }
--- Assets/ContainerA.cs
+++ Assets/ContainerA.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class ContainerA : MonoBehaviour
 {
     public float speed;
+    public float speedIncrease = (float)1 / 480;
+    private int direction = 1;
 
     // Start is called before the first frame update
     void Start()
@@ -15,8 +17,15 @@ public class ContainerA : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        gameObject.transform.position = new Vector2(transform.position.x + speed, transform.position.y);
+        gameObject.transform.position = new Vector2(transform.position.x + (speed * direction), transform.position.y);
 
-        if (transform.position.x >= 10.5 || transform.position.x <= -10.5) { speed *= -1; }
+        // responsible for direction reversal
+        if (transform.position.x >= 10.5 || transform.position.x <= -10.5) { direction *= -1; }
+    }
+
+    // not much point checking WHAT the object is colliding with since there's only one other thing in the scene
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (speed == 0.025) { speed += speedIncrease; }
     }
 }
