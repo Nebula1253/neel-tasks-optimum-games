becd1fc2dd1fb7e5877a56e7f1f6c83db2583a85
Neel Amonkar <s2030247@ed.ac.uk>
2021-07-11 20:33:35 +0530

    Added random direction setting to moving obstacles

--- Assets/Scripts/MovingObstacle.cs
+++ Assets/Scripts/MovingObstacle.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 public class MovingObstacle : MonoBehaviour
 {
     public float setSpeed, speedIncrease;
-    private float speed;
-    private Vector2 direction = Vector2.left;
+    private float speed, initSpeed, speedLimit;
+    private Vector2 direction;
 
     private Vector2 screenBounds;
     private SpriteRenderer rend;
@@ -15,7 +15,11 @@ public class MovingObstacle : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        if (Random.value > 0.5f) { direction = Vector2.right; }
+        else { direction = Vector2.left; }
         speed = 0;
+        initSpeed = setSpeed;
+        speedLimit = setSpeed + 2.5f;
 
         rend = GetComponent<SpriteRenderer>();
 
@@ -37,5 +41,6 @@ public class MovingObstacle : MonoBehaviour
     {
         speed = setSpeed;
         setSpeed += speedIncrease;
+        if (speed == speedLimit) { speed = initSpeed; }
     }
 }
