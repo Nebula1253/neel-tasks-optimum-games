b59e1f54b4466aaf61e80528f1b6623344cadfd1
Neel Amonkar <s2030247@ed.ac.uk>
2021-07-16 21:07:44 +0530

    Started work on task 2 - road fighter clone

---
+++ Assets/Scripts/PlayerCar.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCar : MonoBehaviour
+{
+    public float speed;
+    public Joystick joystick;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float hDirection = Input.GetAxisRaw("Horizontal");
+        float vDirection = Input.GetAxisRaw("Vertical");
+
+        // arrow keys movement for editor testing
+        transform.Translate(Vector2.right * hDirection * Time.deltaTime * speed);
+        transform.Translate(Vector2.up * vDirection * Time.deltaTime * speed);
+
+        // joystick movement for Android
+        transform.Translate(Vector2.right * joystick.Horizontal * Time.deltaTime * speed);
+        transform.Translate(Vector2.up * joystick.Vertical * Time.deltaTime * speed);
+    }
+}
