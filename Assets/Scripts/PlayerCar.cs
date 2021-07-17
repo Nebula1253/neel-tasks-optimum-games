5b7f9e46ab9d468e08bc713f983c52642ebbbce5
Neel Amonkar <s2030247@ed.ac.uk>
2021-07-17 20:44:59 +0530

    started work on track, and implemented accelerate button

--- Assets/Scripts/PlayerCar.cs
+++ Assets/Scripts/PlayerCar.cs
@@ -7,6 +7,8 @@ public class PlayerCar : MonoBehaviour
     public float speed;
     public Joystick joystick;
 
+    private float onRoad = 1;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,11 +22,12 @@ public class PlayerCar : MonoBehaviour
         float vDirection = Input.GetAxisRaw("Vertical");
 
         // arrow keys movement for editor testing
-        transform.Translate(Vector2.right * hDirection * Time.deltaTime * speed);
+        transform.Translate(Vector2.right * hDirection * Time.deltaTime * speed * onRoad);
         transform.Translate(Vector2.up * vDirection * Time.deltaTime * speed);
 
         // joystick movement for Android
-        transform.Translate(Vector2.right * joystick.Horizontal * Time.deltaTime * speed);
+        transform.Translate(Vector2.right * joystick.Horizontal * Time.deltaTime * speed * onRoad);
         transform.Translate(Vector2.up * joystick.Vertical * Time.deltaTime * speed);
+
     }
-}
+    }
--- Assets/Scripts/Track.cs
+++ Assets/Scripts/Track.cs
@@ -1,14 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class Track : MonoBehaviour
 {
-    public float speed;
+    public float speed, speedLimit;
+    private float initSpeed;
     // Start is called before the first frame update
     void Start()
     {
-        
+        initSpeed = speed;
     }
 
     // Update is called once per frame
@@ -16,4 +18,12 @@ public class Track : MonoBehaviour
     {
         transform.Translate(Vector2.down * Time.deltaTime * speed);
     }
+
+    public void OnAcceleratorButtonDown()
+    {
+        speed += 0.05f;
+        if (speed > speedLimit) { speed = speedLimit; }
+    }
+
+    public void OnAcceleratorButtonRelease() { speed = initSpeed; }
 }
