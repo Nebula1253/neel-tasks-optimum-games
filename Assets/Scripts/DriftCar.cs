2c4a3586fdde6c03ac8796f4bd930774858a3c23
Neel Amonkar <s2030247@ed.ac.uk>
2021-08-04 20:53:34 +0530

    added collision to track

--- Assets/Scripts/DriftCar.cs
+++ Assets/Scripts/DriftCar.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 public class DriftCar : MonoBehaviour
 {
     private Rigidbody2D body;
-    public float upwardSpeed, baseAngularSpeed, driftAngularSpeed;
-    private float angularSpeed;
+    public float upwardSpeedLimit, baseAngularSpeed, driftAngularSpeed;
+    public float angularSpeed, upwardSpeed;
     public Joystick joystick;
     private GameObject rotationCenter;
 
@@ -21,11 +21,21 @@ public class DriftCar : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
         float hDirection = Input.GetAxisRaw("Horizontal");
+        float joystickHDirection = 0;
+
+        if (joystick.Horizontal > 0) { joystickHDirection = 1; }
+        else if (joystick.Horizontal < 0) { joystickHDirection = -1; }
+
         transform.RotateAround(rotationCenter.transform.position, Vector3.forward, -hDirection * angularSpeed * Time.deltaTime);
-        transform.RotateAround(rotationCenter.transform.position, Vector3.forward, -joystick.Horizontal * angularSpeed * Time.deltaTime);
+        transform.RotateAround(rotationCenter.transform.position, Vector3.forward, -joystickHDirection * angularSpeed * Time.deltaTime);
+        
         body.velocity = transform.up * upwardSpeed;
+        upwardSpeed+= 10f * Time.deltaTime;
+        if (upwardSpeed > upwardSpeedLimit)
+        {
+            upwardSpeed = upwardSpeedLimit;
+        }
     }
 
     public void driftButtonDown() {
@@ -36,5 +46,10 @@ public class DriftCar : MonoBehaviour
     public void driftButtonRelease() {
         angularSpeed = baseAngularSpeed;
         rotationCenter.GetComponent<CarRotationCenter>().setPosition();
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        upwardSpeed = 0;
     }
 }
